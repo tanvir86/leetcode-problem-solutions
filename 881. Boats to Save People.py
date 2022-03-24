f00176322cba1be2cda087d61e393e391dbd251e
Tanvir Ahmed <ahmed.tanvir.010@gmail.com>
2022-03-24 23:11:34 +0600

    Update 881. Boats to Save People.py

--- 881. Boats to Save People.py
+++ 881. Boats to Save People.py
@@ -1,1 +1,22 @@
-
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+        
+        res, left, right = 0, 0, len(people)-1
+        
+        while  left <= right:
+            # if left == right:
+            #     left += 1
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1
+            else:
+                right -= 1
+                
+            res += 1
+        return res
