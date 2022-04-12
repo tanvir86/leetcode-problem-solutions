7203f61c59d3bebe351d90056aa5eb3f750791bb
Tanvir Ahmed <ahmed.tanvir.010@gmail.com>
2022-04-12 23:17:43 +0600

    Update Game of life to in place replace

--- 289. Game of Life.py
+++ 289. Game of Life.py
@@ -43,6 +43,52 @@ class Solution:
                 
         return board
                     
+        
+        
+    def gameOfLifeInPlace(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        """
+                      \ | /
+        8 directions: - - -  
+                      / | \
+        Clockwise Rotations: (i-1,j-1), (i-1,j), (i-1,j+1),(i, J+1),(i+1,j+1), (i+1,j), (i+1,j-1),(i,j-1)
+        
+        if changed to 0; 1 -> 0; new val 2
+        if changed to 1: 0 -> 1; new val -1
+        """
+        
+        directions = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]]
+        m,n = len(board), len(board[0])
+        
+        for i in range(m):
+            for j in range(n):
+                aliveNgbr = 0
+                
+                for r,c in directions:
+                    nr,nc = i+r, j+c
+                    if nr < 0 or nc < 0 or nr == m or nc == n:
+                        continue
+                    if board[nr][nc] > 0:
+                        aliveNgbr += 1
+                        
+                
+                if board[i][j] == 1:
+                    if aliveNgbr < 2 or aliveNgbr > 3:
+                        board[i][j] = 2
+                elif aliveNgbr == 3:
+                    board[i][j] = -1
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 2:
+                    board[i][j] = 0
+                elif board[i][j] == -1:
+                    board[i][j] = 1
                     
+                
+        return board
                         
                     
