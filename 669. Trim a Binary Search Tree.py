11e6dc06c473966b88467132affe52e9ee2e8f62
Tanvir Ahmed <ahmed.tanvir.010@gmail.com>
2022-04-15 23:39:35 +0600

    BST: 669. Trim a Binary Search Tree

---
+++ 669. Trim a Binary Search Tree.py
@@ -0,0 +1,71 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        
+        # first find the new root
+        # For this we will find the first element whose value is inside the given bound
+        
+        newRoot = root
+        
+        while newRoot is not None and (newRoot.val < low or newRoot.val > high):
+            if newRoot.val < low:
+                newRoot = newRoot.right
+            elif newRoot.val > high:
+                newRoot = newRoot.left
+        
+        if newRoot is None:
+            return None
+        
+        # Now we need to trim the left and right sub tree of this new root
+        
+        # Trimming Left subtree: 
+        #       If we keep going to left we will find the values which is lowest than our lower bound value
+        #       and if a node value is inside our bound then right tree of that node also inside our bound
+        
+        
+        if newRoot.val == low:
+            newRoot.left = None
+        
+        prev = newRoot
+        current = newRoot.left
+        
+        while current is not None:
+            if current.val == low:
+                current.left = None
+                break
+            elif current.val > low:
+                prev = current
+                current = current.left
+            else:
+                current = current.right
+                prev.left = current
+
+        
+        # Trimming right subtree: 
+        #       If we keep going to right we will find the values which is highest than our high bound value
+        #       and if a node value is inside our bound then left tree of that node also inside our bound
+        
+        if newRoot.val == high:
+            newRoot.right = None
+        
+        prev = newRoot
+        current = newRoot.right
+        
+        while current is not None:
+            
+            if current.val == high:
+                current.right = None
+                break
+            elif current.val < high:
+                prev = current
+                current = current.right
+            else:
+                current = current.left
+                prev.right = current
+        
+        return newRoot
